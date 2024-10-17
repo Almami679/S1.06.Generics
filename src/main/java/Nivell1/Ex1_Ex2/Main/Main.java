65b9428202fb3874613c498b6b446f0bb7da3858
Almami989 <almami989@gmail.com>
2024-10-17 09:53:25 +0200

    Revisiones de Raquel Barrio

--- src/main/java/Nivell1/Ex1_Ex2/Main/Main.java
+++ src/main/java/Nivell1/Ex1_Ex2/Main/Main.java
@@ -2,13 +2,15 @@ package Nivell1.Ex1_Ex2.Main;
 
 
 import Nivell1.Ex1_Ex2.Modules.GenericMethods;
+import Nivell1.Ex1_Ex2.Modules.NoGenericMethods;
 import Nivell1.Ex1_Ex2.Modules.Persona;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ////// añadir objeto ex1
+        NoGenericMethods objeto1 = new NoGenericMethods("hierro","oro","carbon");
+        NoGenericMethods objeto1ordenCambiado = new NoGenericMethods("oro", "hierro", "carbon");
 
         GenericMethods<Object> metodo1 = new GenericMethods<>();
 
